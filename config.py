b5a074c18d081f4087dca4804d586f225b99ab2e
PROFESSOR <160086180+Fakecheater77x@users.noreply.github.com>
2024-08-31 09:53:37 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 from os import getenv, path
 from dotenv import load_dotenv
-from data import THE_SHUKLA
+from data import THE_PROFESSOR
 
 
 if path.exists(".env"):
@@ -13,7 +13,7 @@ API_ID = int(getenv("API_ID", None))
 API_HASH = getenv("API_HASH", None)
 SESSION1 = getenv("SESSION1", None)
 BOT_TOKEN = getenv("BOT_TOKEN", None)
-OWNER_ID = list(map(int, getenv("OWNER_ID", "6762113050").split()))
+OWNER_ID = list(map(int, getenv("OWNER_ID", "7463345743").split()))
 
 
 #-------------------------------- OPTIONAL -------------------------------------#
@@ -23,19 +23,19 @@ SESSION3 = getenv("SESSION3")
 SESSION4 = getenv("SESSION4")
 SESSION5 = getenv("SESSION5")
 
-UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/itzshukla/STRANGER-OPUSERBOT")
+UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/Fakecheater77x/PROFESSOR-OPUSERBOT")
 
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 
 GIT_TOKEN = getenv("GIT_TOKEN", None)
 
-EXTRA_IMG = getenv("EXTRA_IMG", "https://telegra.ph/file/aa4bf1e57d11fb75b602e.jpg")
+EXTRA_IMG = getenv("EXTRA_IMG", "https://telegra.ph/file/a37d283f3f1e9712dc7de.jpg")
 
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 
 HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "6919199044").split()))
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "7463345743").split()))
 
 for y in OWNER_ID:
     SUDO_USERS.append(y)
